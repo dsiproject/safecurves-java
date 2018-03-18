9e3045f81cf4f8bbc8b547f2f3a4b2baa380af86
Eric McCorkle <eric@metricspace.net>
2018-03-18 16:55:08 -0400

    Add test framework for Elligator

--- src/main/java/net/metricspace/crypto/math/ec/hash/Elligator1.java
+++ src/main/java/net/metricspace/crypto/math/ec/hash/Elligator1.java
@@ -29,7 +29,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.metricspace.crypto.math.ec;
+package net.metricspace.crypto.math.ec.hash;
 
 import net.metricspace.crypto.math.ec.curve.EdwardsCurve;
 import net.metricspace.crypto.math.ec.point.ECPoint;
